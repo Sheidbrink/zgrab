871b1521b85072182a2c6869e19f6e6746948572
Scott Heidbrink <sheidbri@byu.edu>
2016-08-24 13:02:23 -0600

    Changed setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 apt-get -y install zmap
 apt-get -y install golang
 apt-get -y install git
+apt-get -y install tmux
 mkdir ~/GoPath
 export GOPATH=~/GoPath
 go get github.com/zmap/zgrab
 cd ~/GoPath/src/github.com/zmap/zgrab/
 go build
+cd ~/
+git clone https://github.com/Sheidbrink/zgrab.git
+cd zgrab
+python zcerts.py -w ips.txt
