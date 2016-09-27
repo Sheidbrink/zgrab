b284ba2e88e3896ceb17b0a72de6bce9f45cac2d
root <root@ip-172-31-62-245.ec2.internal>
2016-09-27 21:45:59 +0000

    Added missing dependencies

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-apt-get -y install git
+apt-get -y update
+apt-get -y install git python
+cd ~/
 git clone https://github.com/Sheidbrink/zgrab.git
 cd zgrab
+chmod +x ./setup_ztools.sh
 ./setup_ztools.sh
 python zcerts.py -w ips.txt
