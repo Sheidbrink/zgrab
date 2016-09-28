9877c1a4b840de51a26e8fbcc4904f812d1c1077
root <root@ip-172-31-50-192.ec2.internal>
2016-09-28 20:54:02 +0000

    web server redirect

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 apt-get -y update
+
+apt-get install -y apache2 php libapache2-mod-php
+service httpd start
+update-rc.d apache2 enable
+groupadd www
+usermod -a -G www ubuntu
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
+rm -f /var/www/html/index.html
+echo "<?php header("Location: https://internet.byu.edu"); ?>" > /var/www/html/index.php
+
 apt-get -y install git python
 cd ~/
 git clone https://github.com/Sheidbrink/zgrab.git
